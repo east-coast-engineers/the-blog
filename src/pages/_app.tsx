e892237d5befa55a2004e9d0dc43c8e8b8a833d5
Alexander Bahl <alexanderjbahl@gmail.com>
2023-01-22 22:52:23 -0700

    styles and font setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,22 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { Roboto } from '@next/font/google'
-import Header from  '@/components/Header'
+import HeaderMobile from '@/components/HeaderMobile'
 import Footer from '@/components/Footer'
 
 const roboto = Roboto({
   weight: '400',
-  subsets: ['latin']
+  subsets: ['latin'],
 })
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
+      <HeaderMobile />
       <div className={roboto.className}>
-        <Header />
-        <main className='max-w-screen-2xl mx-auto px-4'>
+        <main className="max-w-screen-2xl px-4">
           <Component {...pageProps} />
         </main>
-        <Footer />
       </div>
     </ThemeProvider>
   )
