73661b49b97c4c0045ff5ff06aefc6b5e0eda1f7
Alexander Bahl <alexanderjbahl@gmail.com>
2023-01-23 20:00:33 -0700

    renamed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { Roboto } from '@next/font/google'
-import HeaderMobile from '@/components/HeaderMobile'
+import Header from '@/components/Header'
 
 const roboto = Roboto({
   weight: '400',
@@ -12,7 +12,7 @@ const roboto = Roboto({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
-      <HeaderMobile />
+      <Header />
       <div className={roboto.className}>
         <main className="max-w-screen-2xl px-4">
           <Component {...pageProps} />
