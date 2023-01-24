b5915eb9caedfad1a0640914914841ae6c1deecb
Alex Bahl <50714034+alexbahl@users.noreply.github.com>
2023-01-23 21:38:40 -0700

    Desktop and Mobile Nav styles (#2)
    * added prettier
    
    * added styled components
    
    * added prettier and styled components
    
    * renamed
    
    * added new icons
    
    sun and moon icons
    
    * styles and font setup
    
    * styles
    
    * added prettier and vscode configs
    
    * start of mobile nav
    
    * installed typescript lint packages
    
    * removed unused footer component
    
    * fixed typing error unicons
    
    * renamed
    
    * renamed
    
    * overflow hidden to hide menu
    
    * renamed
    
    * changed size
    
    * dark styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,64 @@
+import { useState } from 'react'
+import { UilBars, UilTimesCircle } from '@iconscout/react-unicons'
+import styled from 'styled-components'
 import ThemeSwitcher from '@/components/ThemeSwitcher'
 
+interface Props {
+  showMobileMenu: boolean
+}
+
+const NavMenu = styled.div<Props>`
+  position: absolute;
+  width: 100%;
+  height: 90vh;
+  z-index: 50;
+  border-radius: 1rem 1rem 0 0;
+  bottom: ${(props) => (props.showMobileMenu ? '0px' : '-100vh')};
+  transition: bottom 0.5s ease;
+  box-shadow: 0 -4px 15px -3px rgba(0 0 0 / 0.5),
+    0 4px 6px -4px rgb(0 0 0 / 0.5);
+`
+
 export default function Header() {
+  const [navMenu, showNavMenu] = useState(false)
+
   return (
-    <div className="flex justify-between">
-      <span>Chips & Salsa</span>
-      <ThemeSwitcher />
-      <nav>
-        nav link
-      </nav>
-    </div>
+    <>
+      <div
+        className="fixed lg:relative bottom-0 px-4 py-2 w-full border-t lg:border-t-0 dark:border-zinc-800"
+        id="header"
+      >
+        <div className="flex justify-between">
+          <span className="font-bold text-lg self-center text-zinc-900 dark:text-zinc-50">
+            Chips &#38; Salsa
+          </span>
+          <button
+            className="w-11 h-11 flex justify-center place-items-center lg:hidden dark:text-zinc-50"
+            type="button"
+            aria-label="toggle mobile menu"
+            onClick={() => showNavMenu(!navMenu)}
+          >
+            <UilBars size="34" />
+          </button>
+        </div>
+      </div>
+      <NavMenu
+        showMobileMenu={navMenu}
+        className="bg-zinc-50 dark:bg-zinc-800 dark:text-zinc-50"
+      >
+        <div className="h-11 flex flex-row-reverse p-2">
+          <button
+            className="text-zinc-900 dark:text-zinc-50 bg-transparent"
+            onClick={() => showNavMenu(false)}
+          >
+            <UilTimesCircle />
+          </button>
+        </div>
+        <div className="p-4">
+          Menu
+          <ThemeSwitcher />
+        </div>
+      </NavMenu>
+    </>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,21 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'next-themes'
 import { Roboto } from '@next/font/google'
-import Header from  '@/components/Header'
-import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 
 const roboto = Roboto({
   weight: '400',
-  subsets: ['latin']
+  subsets: ['latin'],
 })
-
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider enableSystem={true} attribute="class">
+      <Header />
       <div className={roboto.className}>
-        <Header />
-        <main className='max-w-screen-2xl mx-auto px-4'>
+        <main className="max-w-screen-2xl px-4">
           <Component {...pageProps} />
         </main>
-        <Footer />
       </div>
     </ThemeProvider>
   )
