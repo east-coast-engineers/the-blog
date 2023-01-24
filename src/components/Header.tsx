e58f876d6218c4b56ff63dfed1c175b432257a43
Alexander Bahl <alexanderjbahl@gmail.com>
2023-01-23 20:33:47 -0700

    dark styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { UilBars, UilTimesCircle } from '@iconscout/react-unicons'
 import styled from 'styled-components'
+import ThemeSwitcher from '@/components/ThemeSwitcher'
 
 interface Props {
   showMobileMenu: boolean
@@ -11,7 +12,6 @@ const NavMenu = styled.div<Props>`
   width: 100%;
   height: 90vh;
   z-index: 50;
-  background-color: #f8fafc;
   border-radius: 1rem 1rem 0 0;
   bottom: ${(props) => (props.showMobileMenu ? '0px' : '-100vh')};
   transition: bottom 0.5s ease;
@@ -25,15 +25,15 @@ export default function Header() {
   return (
     <>
       <div
-        className="fixed lg:relative bottom-0 px-4 py-2 w-full border-t lg:border-t-0 border-zinc-300 dark:border-zinc-50"
+        className="fixed lg:relative bottom-0 px-4 py-2 w-full border-t lg:border-t-0 dark:border-zinc-800"
         id="header"
       >
         <div className="flex justify-between">
-          <span className="font-bold text-lg self-center">
+          <span className="font-bold text-lg self-center text-zinc-900 dark:text-zinc-50">
             Chips &#38; Salsa
           </span>
           <button
-            className="w-11 h-11 flex justify-center place-items-center lg:hidden"
+            className="w-11 h-11 flex justify-center place-items-center lg:hidden dark:text-zinc-50"
             type="button"
             aria-label="toggle mobile menu"
             onClick={() => showNavMenu(!navMenu)}
@@ -42,16 +42,22 @@ export default function Header() {
           </button>
         </div>
       </div>
-      <NavMenu showMobileMenu={navMenu}>
+      <NavMenu
+        showMobileMenu={navMenu}
+        className="bg-zinc-50 dark:bg-zinc-800 dark:text-zinc-50"
+      >
         <div className="h-11 flex flex-row-reverse p-2">
           <button
-            className="text-zinc-900 bg-transparent"
+            className="text-zinc-900 dark:text-zinc-50 bg-transparent"
             onClick={() => showNavMenu(false)}
           >
             <UilTimesCircle />
           </button>
         </div>
-        <div className="p-4">Menu</div>
+        <div className="p-4">
+          Menu
+          <ThemeSwitcher />
+        </div>
       </NavMenu>
     </>
   )
